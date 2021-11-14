0d068a7965c01ad9304a10a60bfe9c02e3104c63
Gabriel Rubio <gabrielpinhatarubio@gmail.com>
2021-11-14 19:34:57 -0300

    Aula 6 - Criando o próprio template. utilizando variáveis nos live templates

--- intellij-idea-aula-3/src/br/com/alura/bytebank/Principal.java
+++ intellij-idea-aula-3/src/br/com/alura/bytebank/Principal.java
@@ -2,14 +2,17 @@ package br.com.alura.bytebank;
 
 import br.com.alura.bytebank.io.LeitorCSV;
 import br.com.alura.bytebank.model.Pagamento;
+import org.apache.log4j.Logger;
 
 import java.io.IOException;
 import java.util.List;
 
 public class Principal {
+
+    static final Logger logger = Logger.getLogger(Principal.class);
+
     public static void main(String[] args) throws IOException {
 //        new OpcoesPagamento().mensagemDeBoasVindas();
-
         LeitorCSV leitorCSV = new LeitorCSV();
         List<Pagamento> pagamentos = leitorCSV.ler(1);
 
@@ -17,6 +20,10 @@ public class Principal {
             Double valorComAcerescimo = pagamento.getValor() + (pagamento.getValor()) * 0.10;
             pagamento.setValor(pagamento.getValor() > 100D ? valorComAcerescimo : pagamento.getValor());
             System.out.println(pagamento);
+        }
+
+        if(true){
+            System.out.println("Entrando no ifpronto");
         }
     }
 

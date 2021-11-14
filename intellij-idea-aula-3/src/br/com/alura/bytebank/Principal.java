833770a3a1def6542fedac3c44b2c8e09b1b5622
Gabriel Rubio <gabrielpinhatarubio@gmail.com>
2021-11-14 19:05:56 -0300

    Aula 6 - Utlizando templates

--- intellij-idea-aula-3/src/br/com/alura/bytebank/Principal.java
+++ intellij-idea-aula-3/src/br/com/alura/bytebank/Principal.java
@@ -1,10 +1,23 @@
 package br.com.alura.bytebank;
 
-import br.com.alura.bytebank.visualizador.OpcoesPagamento;
+import br.com.alura.bytebank.io.LeitorCSV;
+import br.com.alura.bytebank.model.Pagamento;
+
+import java.io.IOException;
+import java.util.List;
 
 public class Principal {
-    public static void main(String[] args) {
-        new OpcoesPagamento().mensagemDeBoasVindas();
+    public static void main(String[] args) throws IOException {
+//        new OpcoesPagamento().mensagemDeBoasVindas();
+
+        LeitorCSV leitorCSV = new LeitorCSV();
+        List<Pagamento> pagamentos = leitorCSV.ler(1);
+
+        for (Pagamento pagamento : pagamentos) {
+            Double valorComAcerescimo = pagamento.getValor() + (pagamento.getValor()) * 0.10;
+            pagamento.setValor(pagamento.getValor() > 100D ? valorComAcerescimo : pagamento.getValor());
+            System.out.println(pagamento);
+        }
     }
 
 }
